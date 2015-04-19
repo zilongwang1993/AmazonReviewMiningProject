89351a1c335e8423eeff775664945b06f088f1cb
JayleenZ <zhou.jiayi.1992@gmail.com>
2015-04-19 14:40:15 -0500

    simplepython

--- preprocess.py
+++ preprocess.py
@@ -1,2 +1,24 @@
 #gagagagaga
 #Mike 
+import simplejson
+
+def parse(filename):
+	f = open(filename, 'r')
+	entry = {}
+	for l in f:
+		l = l.strip()
+		colonPos = l.find(':')
+		if colonPos == -1:
+			yield entry
+			entry = {}
+			continue
+		eName = l[:colonPos]
+		rest = l[colonPos+2:]
+		entry[eName] = rest
+	yield entry
+
+
+for e in parse("toy.txt"):
+	print "start"
+	print e["review/helpfulness"]
+	print "end"
