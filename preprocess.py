b5a232e522fd0ffed649a9dd8ef77476e93fe859
saberdoodle <saberdoodle@gmail.com>
2015-04-19 14:41:37 -0500

    countwords done

--- preprocess.py
+++ preprocess.py
@@ -1,2 +1,20 @@
-#gagagagaga
-#Mike 
+import re 	
+
+def countWords(str):
+	count = len(str.split(' '))
+	return count
+
+#f = open('../Books.txt')
+f = open('toy.txt')
+# out =open('toy.text','w')
+# next=f.readline()
+# for i in range(1,1000000):
+# 	out.write(next)
+# 	next = f.readline()
+# out.close()
+# f.close()
+next=f.readline()
+next=f.readline()
+print (countWords(next))
+
+
